e92df485c1f4c46889abace6d76707c315a4ec22
Truong Chau Hien <truongchauhien@gmail.com>
2019-05-06 01:34:12 +0700

    Setup main file.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,32 @@
-import express = require('express')
+import express from 'express';
+import mongoose from 'mongoose';
+import config from './utils/config';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import authRoutes from './routes/auth';
+import productRoutes from './routes/productRoutes';
 
-const PORT:number = Number.parseInt(process.env.NODE_ENV || '3000');
-const app = express();
+async function main() {
+    let mongoDbUri = `mongodb://${config.get('database.host')}:${config.get('database.port')}/${config.get('database.name')}`;
+    mongoose.Promise = global.Promise;
+    try {
+        await mongoose.connect(mongoDbUri, { useNewUrlParser: true });
+    } catch {
+        console.error('Could not connect to MongoDB Server!');
+        return;
+    }
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+    const app = express();
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use(morgan('combined'));
+
+    app.use('/auth', authRoutes);
+    app.use('/product', productRoutes);
+
+    const appPort = config.get('port');
+    app.listen(appPort, () => console.log(`App listening on port ${appPort}`));
+}
+
+main();
