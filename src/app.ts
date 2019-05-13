f12c725414f4d3b556322d09d5161adb2a351025
Truong Chau Hien <truongchauhien@gmail.com>
2019-05-14 02:35:29 +0700

    Apply new routes.

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,12 @@ import config from './utils/config';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
-import authRoutes from './routes/auth';
+import authRoutes from './routes/authRoutes';
 import productRoutes from './routes/productRoutes';
+import categoryRoutes from './routes/categoryRoutes';
+import distributorRoutes from './routes/distributorRoutes';
+import orderRoutes from './routes/orderRoutes';
+import employeeRoutes from './routes/employeeRoutes';
 
 async function main() {
     let mongoDbUri = `mongodb://${config.get('database.host')}:${config.get('database.port')}/${config.get('database.name')}`;
@@ -24,6 +28,10 @@ async function main() {
 
     app.use('/auth', authRoutes);
     app.use('/product', productRoutes);
+    app.use('/category', categoryRoutes);
+    app.use('/distributor', distributorRoutes);
+    app.use('/order', orderRoutes);
+    app.use('/employee', employeeRoutes);
 
     const appPort = config.get('port');
     app.listen(appPort, () => console.log(`App listening on port ${appPort}`));
