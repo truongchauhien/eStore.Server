431278eea7edc9174155fb18c9b3254a58419726
Truong Chau Hien <truongchauhien@gmail.com>
2019-05-14 02:35:16 +0700

    Change order model.

--- src/models/order.ts
+++ src/models/order.ts
@@ -4,14 +4,15 @@ const Schema = mongoose.Schema;
 const OrderSchema = new Schema({
     cashier: {
         type: Schema.Types.ObjectId,
-        ref: 'User'
+        ref: 'Employee'
     },
     totalAmount: Number,
     items: [{
         name: String,
+        upc: String,
         price: Number,
-        priceVat: Number,
-        vat: Number,
+        // priceVat: Number,
+        // vat: Number,
         unit: String,
         quantity: Number
     }],
