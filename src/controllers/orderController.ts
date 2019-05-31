cd76db3f15054a33df2f5a53e45812b7023cddaa
Truong Chau Hien <truongchauhien@gmail.com>
2019-05-31 09:34:35 +0700

    Change product quantity when create order.

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -1,12 +1,20 @@
 import { Request, Response } from "express";
-import OrderModel from "../models/order";
+import orderModel from "../models/order";
+import productModel from '../models/product';
+import moment from 'moment';
+import * as _ from 'lodash';
 
 export const createOrder = async (req: Request, res: Response): Promise<void> => {
     const data = req.body;
 
     let order;
     try {
-        order = await OrderModel.create(data);
+        for (const product of _.get(data, 'items', [])) {
+            const quantity = _.get(product, 'quantity', 0);
+            await productModel.findOneAndUpdate({ upc: product.upc }, { $inc: { quantity: -quantity || 0 } }, { new: true }).lean().exec();
+        }
+        data.createdAt = moment().toDate();
+        order = await orderModel.create(data);
     } catch (ex) {
         res.status(500).json({
             message: 'Internal server error.'
